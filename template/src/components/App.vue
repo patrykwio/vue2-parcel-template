0f785768e343492b3ff6a59877f1eb2dd212c89d
PAUC DOM <patryk@wio.pl>
2019-01-07 21:08:41 +0100

    template folder update

---
+++ template/src/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app" class="app">
+        <h1>{{name}}</h1>
+        <p>{{description}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    };
+</script>
